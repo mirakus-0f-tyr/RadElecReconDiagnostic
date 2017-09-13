163841f43563d5a07c980cc358adf60abeaf8ba9
John M. Davis <jhndavis68@gmail.com>
2017-09-13 16:10:15 -0400

    Updates to ConvMath.java.

--- src/MainMenu/ConvMath.java
+++ src/MainMenu/ConvMath.java
@@ -1,52 +1,61 @@
 // This is a basic class meant to provide conversion methods
 // for US and SI units.  JMD
-
 package MainMenu;
+
 import java.text.DecimalFormat;
 
 public class ConvMath {
-	// expected formatting of various results
-	public static DecimalFormat formatUS_RnC = new DecimalFormat("0.0");
-	public static DecimalFormat formatSI_RnC = new DecimalFormat("0");
-	public static DecimalFormat formatUS_pres = new DecimalFormat("0");
-	public static DecimalFormat formatSI_pres = new DecimalFormat("0");
+    // expected formatting of various results
 
-	// radon units
-	public static String UStoSIRadon(double pCiL) {
-		return formatSI_RnC.format(pCiL * 37);
-	}
+    public static DecimalFormat formatUS_RnC = new DecimalFormat("0.0");
+    public static DecimalFormat formatSI_RnC = new DecimalFormat("0");
+    public static DecimalFormat formatUS_pres = new DecimalFormat("0");
+    public static DecimalFormat formatSI_pres = new DecimalFormat("0");
 
-	public static String SItoUSRadon(int BQM3) {
-		return formatUS_RnC.format((double)BQM3 / 37);
-	}
+    // radon units
+    public static String UStoSIRadon(double pCiL) {
+        return formatSI_RnC.format(pCiL * 37);
+    }
 
-	public static String AverageRadon(double val1, double val2, String unit) {
-		if (unit == "US") 
-			return formatUS_RnC.format((val1 + val2) / 2);
-		else if (unit == "SI") {
-			val1 = Double.parseDouble(UStoSIRadon(val1));
-			val2 = Double.parseDouble(UStoSIRadon(val2));
-			return formatSI_RnC.format((val1 + val2) / 2);
-		}
-		else
-			return "Incorrect unit type specified.";
-	}
+    public static String SItoUSRadon(int BQM3) {
+        return formatUS_RnC.format((double) BQM3 / 37);
+    }
 
-	// temperature units
-	public static String SItoUSTemp(int tempC) {
-		return Integer.toString(tempC * 9 / 5 + 32);
-	}
+    public static String AverageRadon(double val1, double val2, String unit) {
+        if (unit == "US") {
+            return formatUS_RnC.format((val1 + val2) / 2);
+        } else if (unit == "SI") {
+            val1 = Double.parseDouble(UStoSIRadon(val1));
+            val2 = Double.parseDouble(UStoSIRadon(val2));
+            return formatSI_RnC.format((val1 + val2) / 2);
+        } else {
+            return "Incorrect unit type specified.";
+        }
+    }
 
-	public static String UStoSITemp(int tempF) {
-		return Integer.toString((tempF - 32) * 5 / 9);
-	}
+    // temperature units
+    public static String SItoUSTemp(int tempC) {
+        return Integer.toString(tempC * 9 / 5 + 32);
+    }
 
-	// pressure units
-	public static String UStoSIPressure(double inHG) {
-		return formatSI_pres.format(inHG * 33.8639);
-	}
+    public static String UStoSITemp(int tempF) {
+        return Integer.toString((tempF - 32) * 5 / 9);
+    }
 
-	public static String SItoUSPressure(double mBar) {
-		return formatUS_pres.format(mBar * 0.02953);
-	}
+    // pressure units - these aren't used anywhere yet
+    public static String UStoSIPressure(double inHg) {
+        return formatSI_pres.format(inHg * 25.4);
+    }
+
+    public static String SItoUSPressure(double mmHg) {
+        return formatUS_pres.format(mmHg / 25.4);
+    }
+
+    public static String UStoMbar(double inHg) {
+        return formatSI_pres.format(inHg * 33.8639);
+    }
+
+    public static String mBarToUS(double mBar) {
+        return formatUS_pres.format(mBar / 33.8639);
+    }
 }
