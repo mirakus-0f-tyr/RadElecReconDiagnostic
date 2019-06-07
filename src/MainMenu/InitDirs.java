960169075f3b9b6d24f361ec59037a35fd0bd80c
Vavehl <lorin@wmalumni.com>
2019-06-07 12:31:37 -0400

    Only Windows and Mac will have user folders created. More generalized clean-up of InitDirs.java.

--- src/MainMenu/InitDirs.java
+++ src/MainMenu/InitDirs.java
@@ -1,7 +1,10 @@
 package MainMenu;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import javax.swing.JFileChooser;
+
 
 /**
  *
@@ -11,6 +14,7 @@ public class InitDirs {
     
     //Operating System Variables
     public static boolean boolMacOS = false;
+    public static boolean boolWindows = false;
     
     //Directory Variables
     public static File baseDir = new File("ReconDownloadTool");
@@ -19,12 +23,11 @@ public class InitDirs {
     public static File fontsDir = new File("fonts");
     public static File logsDir = new File("logs");
     public static File reportsDir = new File("reports");
+    public static File appDir = new File("");
     
     public static void main() {
         
         //Handle logging first...
-        logsDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool"+File.separator+"logs") : new File("logs");
-        logsDir = boolMacOS==false ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool"+File.separator+"logs") : new File("logs");
 	if (!logsDir.exists()) {
             System.out.println("Logs directory does not exist. Creating...");
             logsDir.mkdirs();
@@ -34,41 +37,94 @@ public class InitDirs {
         //Determine OS before checking for config/data/reports folders...
         findOperatingSystem();
         
-	// check existence of critical directories before proceeding
+        //Determine application (JAR file) directory.
+        showAppDirectory();
         
-	configDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool"+File.separator+"config") : new File("config");
-        configDir = boolMacOS==false ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool"+File.separator+"config") : new File("config");
+	// check existence of critical directories before proceeding, and copy exiting config files to user directories (if applicable).
+        assignWorkingDirectories();
+        
 	if (!configDir.exists()) {
-            Logging.main(configDir.toString());
             Logging.main("Config directory does not exist.  Creating...");
             configDir.mkdirs();
+            copySourceFileToUserDirectory("config",configDir,"company.txt");
+            copySourceFileToUserDirectory("config",configDir,"config.txt");
+            copySourceFileToUserDirectory("config",configDir,"deployment.txt");
+            copySourceFileToUserDirectory("config",configDir,"report.txt");
+            copySourceFileToUserDirectory("config",configDir,"signature.png");
+            copySourceFileToUserDirectory("config",configDir,"signature.jpg"); //...just in case somebody had added a JPEG.
         }
-	dataDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool"+File.separator+"data") : new File("data");
-        dataDir = boolMacOS==false ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool"+File.separator+"data") : new File("data");
 	if (!dataDir.exists()) {
             Logging.main("Data directory does not exist.  Creating...");
             dataDir.mkdirs();
         }
-        fontsDir = boolMacOS==true ? new File(new File("ReconDownloadTool.app").getAbsolutePath() + File.separator + "Contents" + File.separator + "Java" + File.separator + "fonts") : new File("fonts");
 	if (!fontsDir.exists()) {
             Logging.main("WARNING: Fonts directory is not found or does not exist.");
             Logging.main("WARNING: Attempted Fonts directory = " + fontsDir);
         }
-	reportsDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool"+File.separator+"reports") : new File("reports");
-        reportsDir = boolMacOS==false ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool"+File.separator+"reports") : new File("reports");
 	if (!reportsDir.exists()) {
             Logging.main("Reports directory does not exist.  Creating...");
             reportsDir.mkdirs();
         }
-        
-        //baseDir is only used in macOS, to place graph.png
-        baseDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool") : new File("ReconDownloadTool");
-        baseDir = boolMacOS==false ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool") : new File("ReconDownloadTool");
     }
     
     public static void findOperatingSystem() {
         String strOperatingSystem = System.getProperty("os.name");
         Logging.main("Operating System: " + strOperatingSystem);
         boolMacOS = strOperatingSystem.startsWith("Mac");
+        boolWindows = strOperatingSystem.contains("Win");
+    }
+    
+    public static void showAppDirectory() {
+        try {
+            System.out.println("Application Directory (JAR file location) = " + appDir.getAbsolutePath());
+        } catch (Exception ex) {
+            Logging.main("ERROR: Unable to find application directory!");
+        }
+    }
+    
+    public static void copySourceFileToUserDirectory(String strSource, File fileDestination, String strFileName) {
+        try {
+            File sourceDir = new File(strSource + File.separator + strFileName);
+            if (sourceDir.exists()) {
+                File targetDir = new File(fileDestination.toString() + File.separator + strFileName);
+                Files.copy(sourceDir.toPath(), targetDir.toPath());
+            } else {
+                Logging.main("WARNING: Unable to resolve application source directory in copySourceFileToUserDir (" + strSource + File.separator + strFileName + ")!");
+            }
+        } catch (IOException ex) {
+            Logging.main("ERROR: copySourceFileToUserDir!");
+        }
+    }
+    
+    public static void assignWorkingDirectories() {
+        //Make sure that findOperatingSystem() has been called at least once before running this.
+        //Assigns proper paths for configDir, dataDir, reportsDir, baseDir, and fontsDir
+        //baseDir is used to place graph.png (and provide an easy way to find the user's created RDT files).
+        try {
+            if(boolMacOS==true) {
+                baseDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool") : new File("ReconDownloadTool");
+                configDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool"+File.separator+"config") : new File("config");
+                dataDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool"+File.separator+"data") : new File("data");
+                fontsDir = boolMacOS==true ? new File(new File("ReconDownloadTool.app").getAbsolutePath() + File.separator + "Contents" + File.separator + "Java" + File.separator + "fonts") : new File("fonts");
+                logsDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool"+File.separator+"logs") : new File("logs");
+                reportsDir = boolMacOS==true ? new File(System.getProperty("user.home")+File.separator+"Documents"+File.separator+"ReconDownloadTool"+File.separator+"reports") : new File("reports");
+            }else if(boolWindows==true) {
+                baseDir = boolWindows==true ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool") : new File("ReconDownloadTool");
+                configDir = boolWindows==true ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool"+File.separator+"config") : new File("config");
+                dataDir = boolWindows==true ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool"+File.separator+"data") : new File("data");
+                fontsDir = new File("fonts");
+                logsDir = boolWindows==true ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool"+File.separator+"logs") : new File("logs");
+                reportsDir = boolWindows==true ? new File(new JFileChooser().getFileSystemView().getDefaultDirectory().toString()+File.separator+"ReconDownloadTool"+File.separator+"reports") : new File("reports");
+            }else {
+                baseDir = new File("ReconDownloadTool");
+                configDir = new File("config");
+                dataDir = new File("data");
+                fontsDir = new File("fonts");
+                logsDir = new File("logs");
+                reportsDir = new File("reports");
+            }
+        } catch (Exception ex) {
+            Logging.main("ERROR: assignWorkingDirectories!");
+        }
     }
 }
